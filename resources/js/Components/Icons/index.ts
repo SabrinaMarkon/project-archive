70b5115d2c7b751aa52fd2943e986e4ef11dfcca
Sabrina Markon <phpsitescripts@outlook.com>
2025-11-16 23:19:44 -0800

    Add tech stack icon components

---
+++ resources/js/Components/Icons/index.ts
@@ -0,0 +1,6 @@
+export { default as LaravelIcon } from './LaravelIcon';
+export { default as ReactIcon } from './ReactIcon';
+export { default as TypeScriptIcon } from './TypeScriptIcon';
+export { default as MySQLIcon } from './MySQLIcon';
+export { default as TailwindIcon } from './TailwindIcon';
+export { default as JavaScriptIcon } from './JavaScriptIcon';
